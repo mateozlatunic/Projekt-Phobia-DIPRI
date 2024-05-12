d54f07f5d8e893c37982a99067e5975bbe59d3ac
Marko Galavić <marcogalavic7@gmail.com>
2024-05-12 13:46:27 +0200

    HealthBar & Death Screen
    Dodani su health bar i njegove funkcionalnosti i death screen sa funkcionalnostima povratka na main menu ili restart

---
+++ Projekt-Phobia/Assets/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public GameObject hud;
+    //public GameObject inv;
+    public GameObject deathScreen;
+    public GameObject player;
+
+    public float health = 100f;
+
+
+
+    void Start()
+    {
+        deathScreen.SetActive(false);
+    }
+
+
+
+    void Update()
+    {
+
+        if(health <= 0)
+        {
+            player.SetActive(false);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            hud.SetActive(false);
+            //inv.SetActive(false);
+            deathScreen.SetActive(true);
+        }
+
+        if (health > 100)
+        {
+            health = 100;
+        }
+        
+    }
+}
