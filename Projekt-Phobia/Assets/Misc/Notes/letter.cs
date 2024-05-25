5e0c81aeee8f75e828ee75cd8dd11d38624076ee
Marko <105108389+MrKMark202@users.noreply.github.com>
2024-05-25 16:43:42 +0200

    Male promjene
    dodana nova vrata i interaktanje sa porukama

--- Projekt-Phobia/Assets/Misc/Notes/letter.cs
+++ Projekt-Phobia/Assets/Misc/Notes/letter.cs
@@ -6,15 +6,33 @@ public class letter : MonoBehaviour
 {
     public GameObject letterUI;
     bool toggle;
+    public Renderer letterMesh;
 
-    public void opetCloseLetter()
+    void Start()
+    {
+        // Automatski pronađi Renderer na ovom GameObject-u ako nije postavljen
+        if (letterMesh == null)
+        {
+            letterMesh = GetComponent<Renderer>();
+        }
+
+        // Provjera ako letterMesh nije pronađen
+        if (letterMesh == null)
+        {
+            Debug.LogError("letterMesh is not assigned and no Renderer found on the GameObject.");
+        }
+    }
+
+    public void openCloseLetter()
     {
         toggle = !toggle;
         if(toggle == false) {
             letterUI.SetActive(false);
+            letterMesh.enabled = true;
         }
         if(toggle == true) {
             letterUI.SetActive(true);
+            letterMesh.enabled = false;
         }
     }
 }
